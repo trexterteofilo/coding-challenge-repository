189d1e793f88cf5fe1bcf77b3049955095080bd5
trexterteofilo <trexterlynford2288@gmail.com>
2024-08-21 21:23:59 +0800

    problem_set_2 : Valid Parenthesis

--- coding-challenge-repository/Program.cs
+++ coding-challenge-repository/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using coding_challenge_repository.problem_set_1;
+using coding_challenge_repository.problem_set_2;
 
 namespace coding_challenge_repository
 {
@@ -12,10 +13,15 @@ namespace coding_challenge_repository
         static void Main(string[] args)
         {
             PalindromeChecker palindromeChecker = new PalindromeChecker();
+            ParenthesisChecker parenthesisChecker = new ParenthesisChecker();
 
             //Problem set 1 Palindrome Pairs
             string[] words = { "bat", "tab", "cat" };
             List<List<int>> result = palindromeChecker.PalindromePairs(words);
+
+            //Problem set 2 Parentheses Checker
+            string inputString = "()[]{}";
+            Console.WriteLine(parenthesisChecker.IsValid(inputString));
         }
     }
 }
---
+++ coding-challenge-repository/problem_set_2/problem_set_2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge_repository.problem_set_2
+{ 
+    class ParenthesisChecker
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            Dictionary<char, char> bracketPairs = new Dictionary<char, char>()
+            {
+                { ')', '(' },
+                { ']', '[' },
+                { '}', '{' }
+            };
+
+            foreach (char c in s)
+            {
+                if (bracketPairs.ContainsKey(c))
+                {
+                    char topElement = stack.Count > 0 ? stack.Pop() : '#';
+
+                    if (topElement != bracketPairs[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            //Display
+            Console.WriteLine("Parenthesis Checker \n");
+            Console.WriteLine("Input String: " + s);
+            Console.Write("Result: ");
+
+            return stack.Count == 0;
+        }
+    }
+}
