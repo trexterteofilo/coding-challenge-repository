8c60dcc9d9c33485dab7569ec6f420a22638194d
trexterteofilo <trexterlynford2288@gmail.com>
2024-08-21 21:20:44 +0800

    problem_set_1 : PlaindromePair

--- coding-challenge-repository/Program.cs
+++ coding-challenge-repository/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using coding_challenge_repository.problem_set_1;
 
 namespace coding_challenge_repository
 {
@@ -10,6 +11,11 @@ namespace coding_challenge_repository
     {
         static void Main(string[] args)
         {
+            PalindromeChecker palindromeChecker = new PalindromeChecker();
+
+            //Problem set 1 Palindrome Pairs
+            string[] words = { "bat", "tab", "cat" };
+            List<List<int>> result = palindromeChecker.PalindromePairs(words);
         }
     }
 }
---
+++ coding-challenge-repository/problem_set_1/problem_set_1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_challenge_repository.problem_set_1
+{
+    public class PalindromeChecker
+    {
+        public bool IsPalindrome(string s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                    return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        public List<List<int>> PalindromePairs(string[] words)
+        {
+            var result = new List<List<int>>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        string concatenated = words[i] + words[j];
+                        if (IsPalindrome(concatenated))
+                        {
+                            result.Add(new List<int> { i, j });
+                        }
+                    }
+                }
+            }
+
+            //Display
+            Console.WriteLine("Parenthesis Checker \n");
+            string wordsArray = string.Join(", ", words);
+            Console.WriteLine("Input string: " + wordsArray);
+            foreach (var pair in result)
+            {
+                Console.WriteLine($"[{pair[0]}, {pair[1]}]");
+            }
+            Console.WriteLine("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+
+            return result;
+        }
+
+    }
+}
